a0795b03bac1df1822041809c334ff7a3938e4ba
Enableees <vladshumkov@bk.ru>
2026-03-09 16:37:49 +0800

    Добавление логики и привязка ее к графическому интерфейсу

--- WinFormsApp2/Form1.cs
+++ WinFormsApp2/Form1.cs
@@ -5,11 +5,55 @@ namespace WinFormsApp2
         public Form1()
         {
             InitializeComponent();
+
+            textBox1.Text = Properties.Settings.Default.input.ToString();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            string input = textBox1.Text;
+            Properties.Settings.Default.input = input;
+            Properties.Settings.Default.Save();
 
+            if (!int.TryParse(input, out int number))
+            {
+                MessageBox.Show("Ошибка: введите целое число");
+                return;
+            }
+
+            string result = Logic.ConvertToRublesAndKopecks(number);
+            MessageBox.Show(result);
+        }
+        public class Logic
+        {
+            public static string ConvertToRublesAndKopecks(int number)
+            {
+                if (number < 1 || number > 9999)
+                {
+                    return "Некорректное значение. Введите число от 1 до 9999";
+                }
+
+                int rubles = number / 100;
+                int kopecks = number % 100;
+
+                string rubleWord;
+                if (rubles % 10 == 1 && rubles % 100 != 11)
+                    rubleWord = "рубль";
+                else if (rubles % 10 >= 2 && rubles % 10 <= 4 && (rubles % 100 < 10 || rubles % 100 >= 20))
+                    rubleWord = "рубля";
+                else
+                    rubleWord = "рублей";
+
+                string kopeckWord;
+                if (kopecks % 10 == 1 && kopecks % 100 != 11)
+                    kopeckWord = "копейка";
+                else if (kopecks % 10 >= 2 && kopecks % 10 <= 4 && (kopecks % 100 < 10 || kopecks % 100 >= 20))
+                    kopeckWord = "копейки";
+                else
+                    kopeckWord = "копеек";
+
+                return $"{rubles} {rubleWord} {kopecks} {kopeckWord}";
+            }
         }
     }
 }
